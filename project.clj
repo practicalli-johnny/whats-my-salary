6f194d99ff8d1f31902712610ffe549c23068e69
John Stevenson <john@jr0cket.co.uk>
2013-08-29 15:40:20 +0100

    bumped to Clj 1.4, created coloured custom prompt

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://blog.jr0cket.co.uk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :main whats-my-salary.core)
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :main whats-my-salary.core
+  :repl-options { ;; Specify the string to print when prompting for input.
+                 :prompt (fn [ns] (str "\u001B[35m[\u001B[34m" ns "\u001B[35m]\u001B[33mcλ:\u001B[m " ))
+                 ;; What to print when the repl session starts.
+                 :welcome (println "Time for  REPL Driven Development!")}
+ )
